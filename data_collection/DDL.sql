6a57b14f760428067fbc956ad1ce6703a0a6c269
Woo Jung <wooseokjung0826@gmail.com>
2018-11-17 00:39:58 -0800

    created DDL.sql with table definitions and modified scrape_initial.py to scrape all specified stocks and INSERT to our database

--- data_collection/DDL.sql
+++ data_collection/DDL.sql
@@ -3,21 +3,58 @@
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
 
--- drop these tables from schema if they already exist
-DROP TABLE if exists Symbol;
-DROP TABLE if exists BTC;
+drop table if exists BTC;
+drop table if exists BCH;
+drop table if exists ETH;
+drop table if exists EOS;
+drop table if exists XRP;
 
-CREATE TABLE Symbol (
-	symbol TEXT NOT NULL PRIMARY KEY CHECK(len(symbol) <= 5),
-	name TEXT NOT NULL
-);
-
-CREATE TABLE BTC (
+create table BTC (
 	date TEXT PRIMARY KEY,
 	open REAL NOT NULL,
 	high REAL NOT NULL,
 	low REAL NOT NULL,
 	close REAL NOT NULL,
 	adjClose REAL NOT NULL,
-	volume INTEGER CHECK (volume >= 0)
+	volume INTEGER
+);
+
+create table BCH (
+	date TEXT PRIMARY KEY,
+	open REAL NOT NULL,
+	high REAL NOT NULL,
+	low REAL NOT NULL,
+	close REAL NOT NULL,
+	adjClose REAL NOT NULL,
+	volume INTEGER
+);
+
+create table ETH (
+	date TEXT PRIMARY KEY,
+	open REAL NOT NULL,
+	high REAL NOT NULL,
+	low REAL NOT NULL,
+	close REAL NOT NULL,
+	adjClose REAL NOT NULL,
+	volume INTEGER
+);
+
+create table EOS (
+	date TEXT PRIMARY KEY,
+	open REAL NOT NULL,
+	high REAL NOT NULL,
+	low REAL NOT NULL,
+	close REAL NOT NULL,
+	adjClose REAL NOT NULL,
+	volume INTEGER
+);
+
+create table XRP (
+	date TEXT PRIMARY KEY,
+	open REAL NOT NULL,
+	high REAL NOT NULL,
+	low REAL NOT NULL,
+	close REAL NOT NULL,
+	adjClose REAL NOT NULL,
+	volume INTEGER
 );
--- data_collection/scrape_initial.py
+++ data_collection/scrape_initial.py
@@ -4,66 +4,98 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import sqlite3
 
 # constants
 SCROLL_PAUSE_TIME = 0.5
 
 # list of stocks
 stocks = ['BTC','BCH','ETH','EOS','XRP']
-stocks = [s + '-USD' for s in stocks]
+stocksUSD = [s + '-USD' for s in stocks]
 
 print (stocks)
 
+# constants
 url = 'https://finance.yahoo.com/quote/ETH-USD/history?p=ETH-USD'
+sqlite_file = './stocks.db'
+
+# sqlite db connection
+connection = sqlite3.connect(sqlite_file)
+cursor = connection.cursor()
+
+# webdriver
 browser = webdriver.Chrome()
-browser.get(url)
 
-time.sleep(2)
-print ("wake")
+def scrape(browser, cursor, stock):
 
-### Filter Max duration and apply
-# click date interval selection
-browser.find_elements_by_xpath("//span[contains(@class, 'C($c-fuji-blue-1-b)') and contains(@class, 'Mstart(8px)') and contains(@class, 'Cur(p)')]")[0].click()
-print ("clicked!")
-# select max interval
-browser.find_elements_by_xpath("//span[contains(@class, 'P(5px)') and contains(@class, 'W(37px)')]")[7].click()
-print ("clicked MAX")
-time.sleep(3)
-# select Done
-browser.find_element_by_xpath("//button[contains(@class, 'Miw(80px)!') and contains(@class, 'Fl(start)')]").click()
-print ("clicked Done")
-# select Apply
-browser.find_element_by_xpath("//button[contains(@class, 'Py(9px)') and contains(@class, 'Fl(end)')]").click()
-print ("clicked Apply")
-time.sleep(2)
+	stockUSD = stock + "-USD"
 
-# count number of page down scrolls
-countScroll = 0;
+	url = ('https://finance.yahoo.com/quote/%s/history?p=%s' % (stockUSD,stockUSD))
+	browser.get(url)
+	# SQLite relations
+	table_name = stock
 
-length_page = browser.execute_script("window.scrollTo(0, document.documentElement.scrollHeight); var lenPage=document.documentElement.scrollHeight; return lenPage;")
-print("scroll!")
+	time.sleep(2)
+	print('loading', end='', flush=True)
+	### Filter Max duration and apply
+	# click date interval selection
+	browser.find_elements_by_xpath("//span[contains(@class, 'C($c-fuji-blue-1-b)') and contains(@class, 'Mstart(8px)') and contains(@class, 'Cur(p)')]")[0].click()
+	print('.', end='', flush=True)
+	time.sleep(0.5)
+	# select max interval
+	browser.find_elements_by_xpath("//span[contains(@class, 'P(5px)') and contains(@class, 'W(37px)')]")[7].click()
+	print('.', end='', flush=True)
+	time.sleep(0.5)
+	# select Done
+	browser.find_element_by_xpath("//button[contains(@class, 'Miw(80px)!') and contains(@class, 'Fl(start)')]").click()
+	print('.', end='', flush=True)
+	time.sleep(0.5)
+	# select Apply
+	browser.find_element_by_xpath("//button[contains(@class, 'Py(9px)') and contains(@class, 'Fl(end)')]").click()
+	print('.', flush=True)
+	time.sleep(1)
+	print('loaded')
 
-while True:
-	last_length = length_page
-	time.sleep(SCROLL_PAUSE_TIME)
+	# count number of page down scrolls
+	countScroll = 0;
+
 	length_page = browser.execute_script("window.scrollTo(0, document.documentElement.scrollHeight); var lenPage=document.documentElement.scrollHeight; return lenPage;")
-	countScroll = countScroll + 1
-	if last_length==length_page:
-		break
-	print("scroll!")
-print("Total scrolls to bottom of page: %d" % countScroll)
+	print("scrolling", end='', flush=True)
 
-response = browser.page_source
+	while True:
+		last_length = length_page
+		time.sleep(SCROLL_PAUSE_TIME)
+		length_page = browser.execute_script("window.scrollTo(0, document.documentElement.scrollHeight); var lenPage=document.documentElement.scrollHeight; return lenPage;")
+		countScroll = countScroll + 1
+		print('.', end='', flush=True)
+		if last_length==length_page:
+			print('')
+			break
+	print("Total scrolls to bottom of page: %d" % countScroll)
+
+	response = browser.page_source
+
+	# parse wih BeautifulSoup
+	soup = BeautifulSoup(response, features="html.parser")
+
+	# for each data row, strip all but the text
+	for row in soup.find_all('tr', {'class' : 'BdT Bdc($c-fuji-grey-c) Ta(end) Fz(s) Whs(nw)'}):
+		tup = "('"+row.contents[0].text+"'"
+		for i in range(1,len(row)):
+			c = row.contents[i].text
+			if c=="-":
+				c = 'null'
+			tup = tup + "," + c.replace(',','')
+		tup += ")"
+		print("insert or ignore into %s values %s" % (stock,tup))
+		cursor.execute("insert or ignore into %s values %s" % (stock,tup))
+
+for stock in stocks:
+	scrape(browser, cursor,stock)
+
+# quit browser gracefully
 browser.quit()
 print ("browser quit")
-
-# parse wih BeautifulSoup
-soup = BeautifulSoup(response, features="html.parser")
-
-# for each data row, strip all but the text
-for row in soup.find_all('tr', {'class' : 'BdT Bdc($c-fuji-grey-c) Ta(end) Fz(s) Whs(nw)'}):
-	#print (row.contents)
-	temp = "'"+row.contents[0].text+"'"
-	for i in range(2,len(row)):
-		temp = temp + "," + row.contents[i].text.replace(',','')
-	print(temp)
+# commit changes and close file
+connection.commit()
+connection.close
