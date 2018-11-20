ddacc9fd134dcd6efe561e7f21e54ad9dcbb535d
Woo Jung <wooseokjung0826@gmail.com>
2018-11-19 21:13:26 -0800

    updates. added a scrape_test.py for unit testing scraping algorithm

--- data_collection/scrape_initial.py
+++ data_collection/scrape_initial.py
@@ -6,116 +6,126 @@ from selenium.webdriver.common.by import By
 import time
 import sqlite3
 
-# constants
-SCROLL_PAUSE_TIME = 0.5
-SQLITE_FILE ='./stocks.db'
-URL= 'https://finance.yahoo.com/quote/%s/history?p=%s'
+# scraper class
+class scraper:
 
-# list of stocks
-stocks = ['BTC','BCH','ETH','EOS','XRP']
-# stocksUSD = [s + '-USD' for s in stocks]
+	def startBrowser():
+		browser = webdriver.Chrome()
+		return browser
 
-# sqlite db connection
-connection = sqlite3.connect(SQLITE_FILE)
-cursor = connection.cursor()
+	def stockURL(template, symb):
+		symb += '-USD'
+		return (template % (symb, symb))
 
-# webdriver
-browser = webdriver.Chrome()
-
-def stockURL(template, symb):
-	symb += '-USD'
-	return (template % (symb, symb))
-
-def filtering(driver):
-	time.sleep(2)
-	print('loading', end='', flush=True)
-	### Filter Max duration and apply
-	# click date interval selection
-	driver.find_elements_by_xpath("//span[contains(@class, 'C($c-fuji-blue-1-b)') and contains(@class, 'Mstart(8px)') and contains(@class, 'Cur(p)')]")[0].click()
-	print('.', end='', flush=True)
-	time.sleep(0.5)
-	# select max interval
-	driver.find_elements_by_xpath("//span[contains(@class, 'P(5px)') and contains(@class, 'W(37px)')]")[7].click()
-	print('.', end='', flush=True)
-	time.sleep(0.5)
-	# select Done
-	driver.find_element_by_xpath("//button[contains(@class, 'Miw(80px)!') and contains(@class, 'Fl(start)')]").click()
-	print('.', end='', flush=True)
-	time.sleep(0.5)
-	# select Apply
-	driver.find_element_by_xpath("//button[contains(@class, 'Py(9px)') and contains(@class, 'Fl(end)')]").click()
-	print('.', end='', flush=True)
-	time.sleep(1)
-	print('loaded')
-
-def scrollPage(driver, pause_time):
-	# count number of page down scrolls
-	countScroll = 0;
-
-	length_page = driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight); var lenPage=document.documentElement.scrollHeight; return lenPage;")
-	print("scrolling", end='', flush=True)
-
-	while True:
-		last_length = length_page
-		time.sleep(pause_time)
-		length_page = driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight); var lenPage=document.documentElement.scrollHeight; return lenPage;")
-		countScroll = countScroll + 1
+	def filtering(driver):
+		time.sleep(2)
+		print('loading', end='', flush=True)
+		### Filter Max duration and apply
+		# click date interval selection
+		driver.find_elements_by_xpath("//span[contains(@class, 'C($c-fuji-blue-1-b)') and contains(@class, 'Mstart(8px)') and contains(@class, 'Cur(p)')]")[0].click()
 		print('.', end='', flush=True)
-		if last_length==length_page:
-			print('')
-			break
-	print("Total scrolls to bottom of page: %d" % countScroll)
+		time.sleep(0.5)
+		# select max interval
+		driver.find_elements_by_xpath("//span[contains(@class, 'P(5px)') and contains(@class, 'W(37px)')]")[7].click()
+		print('.', end='', flush=True)
+		time.sleep(0.5)
+		# select Done
+		driver.find_element_by_xpath("//button[contains(@class, 'Miw(80px)!') and contains(@class, 'Fl(start)')]").click()
+		print('.', end='', flush=True)
+		time.sleep(0.5)
+		# select Apply
+		driver.find_element_by_xpath("//button[contains(@class, 'Py(9px)') and contains(@class, 'Fl(end)')]").click()
+		print('.', end='', flush=True)
+		time.sleep(1)
+		print('loaded')
 
-	response = driver.page_source
-	return response
+	def scrollPage(driver, pause_time):
+		# count number of page down scrolls
+		countScroll = 0;
 
-def parsePage(page):
-	return BeautifulSoup(page, features='html.parser')
+		length_page = driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight); var lenPage=document.documentElement.scrollHeight; return lenPage;")
+		print("scrolling", end='', flush=True)
 
-def updateData(soup, cursor,rel_name):
-	# for each data row, strip all but the text
-	for row in soup.find_all('tr', {'class' : 'BdT Bdc($c-fuji-grey-c) Ta(end) Fz(s) Whs(nw)'}):
-		tup = "('"+row.contents[0].text+"'"
-		for i in range(1,len(row)):
-			c = row.contents[i].text
-			if c=="-":
-				c = 'null'
-			tup = tup + "," + c.replace(',','')
-		tup += ")"
-		cursor.execute("insert or ignore into %s values %s" % (rel_name,tup))
-		print ('>insert into %s' % rel_name)
+		while True:
+			last_length = length_page
+			time.sleep(pause_time)
+			length_page = driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight); var lenPage=document.documentElement.scrollHeight; return lenPage;")
+			countScroll = countScroll + 1
+			print('.', end='', flush=True)
+			if last_length==length_page:
+				print('')
+				break
+		print("Total scrolls to bottom of page: %d" % countScroll)
 
-# script run!!
-for stock in stocks:
-	print('initiating scrape for stock: %s' % stock)
+		response = driver.page_source
+		return response
 
-	# browser points to stock URL
-	surl = stockURL(URL,stock)
-	browser.get(surl)
-	print('browser launched')
+	def parsePage(page):
+		return BeautifulSoup(page, features='html.parser')
 
-	# SQLite table name
-	table_name = stock
+	def updateData(soup, cursor,rel_name):
+		# for each data row, strip all but the text
+		for row in soup.find_all('tr', {'class' : 'BdT Bdc($c-fuji-grey-c) Ta(end) Fz(s) Whs(nw)'}):
+			tup = "('"+row.contents[0].text+"'"
+			for i in range(1,len(row)):
+				c = row.contents[i].text
+				if c=="-":
+					c = 'null'
+				tup = tup + "," + c.replace(',','')
+			tup += ")"
+			cursor.execute("insert or ignore into %s (date,open,high,low,close,adjClose,volume) values %s" % (rel_name,tup))
+			print ('>insert into %s' % rel_name)
 
-	# filter max interval
-	filtering(browser)
+if __name__ == '__main__':
+	# sqlite db connection
+	SQLITE_FILE ='./database/stocks.db'
+	connection = sqlite3.connect(SQLITE_FILE)
+	cursor = connection.cursor()
 
-	# scroll to bottom of page
-	response = scrollPage(browser, SCROLL_PAUSE_TIME)
+	# webdriver
+	browser = scraper.startBrowser()
 
-	# parse the response
-	parsed = parsePage(response)
+	# constants
+	SCROLL_PAUSE_TIME = 0.5
 
-	# update the database
-	updateData(parsed, cursor, stock)
-	print('database update for %s completed' % stock)
+	# list of stocks
+	stocks = ['BTC','BCH','ETH','EOS','XRP']
 
-# commit changes and close file
-connection.commit()
-print('database changes committed')
-connection.close()
-print('database connection closed')
+	# template URL
+	URL= 'https://finance.yahoo.com/quote/%s/history?p=%s'
+	
+	# iterate for all stocks
+	for stock in stocks:
+		print('initiating scrape for stock: %s' % stock)
 
-# quit browser gracefully
-browser.quit()
-print('browser exit')
+		# browser points to stock URL
+		surl = scraper.stockURL(URL,stock)
+		print('headin to ' + surl)
+		browser.get(surl)
+		print('browser launched')
+
+		# SQLite table name
+		table_name = stock
+
+		# filter max interval
+		scraper.filtering(browser)
+
+		# scroll to bottom of page
+		response = scraper.scrollPage(browser, SCROLL_PAUSE_TIME)
+		
+		# parse the response
+		parsed = scraper.parsePage(response)
+
+		# update the database
+		scraper.updateData(parsed, cursor, stock)
+		print('database update for %s completed' % stock)
+
+	# commit changes and close file
+	connection.commit()
+	print('database changes committed')
+	connection.close()
+	print('database connection closed')
+
+	# quit browser gracefully
+	browser.quit()
+	print('browser exit')
---
+++ data_collection/scrape_test.py
@@ -0,0 +1,63 @@
+# Unit-test for scrape_initial.py
+# steps: 
+### 1) install required package dependencies: 
+##########`pip3 install -U -r requirements.txt`
+### 2) run scrape_test.py
+##########`python3 scrape_test.py`
+#
+#
+# @author Woo Jung
+# @version 1.0
+from scrape_initial import scraper
+import unittest
+
+class TestScrapeFunctions(unittest.TestCase):
+
+	# template URL
+	URL= 'https://finance.yahoo.com/quote/%s/history?p=%s'
+	# list of stocks
+	stocks = ['BTC','BCH','ETH','EOS','XRP']
+
+	URL_BTC='https://finance.yahoo.com/quote/BTC-USD/history?p=BTC-USD'
+	URL_ETH='https://finance.yahoo.com/quote/ETH-USD/history?p=ETH-USD'
+	URL_XRP='https://finance.yahoo.com/quote/XRP-USD/history?p=XRP-USD'
+
+	# test stockURL() generations
+	def test_stockURL_BTC(self):
+		self.assertEqual(scraper.stockURL(self.URL,'BTC'), self.URL_BTC)
+
+	def test_stockURL_ETH(self):
+		self.assertEqual(scraper.stockURL(self.URL,'ETH'), self.URL_ETH)
+
+	def test_stockURL_XRP(self):
+		self.assertEqual(scraper.stockURL(self.URL,'XRP'), self.URL_XRP)
+
+	def test_selenium(self):
+		# init browser
+		browser = scraper.startBrowser()
+		# head to BTC page
+		browser.get(self.URL_BTC)
+
+		# should be this URL
+		want_URL = 'https://finance.yahoo.com/quote/BTC-USD/history?period1=1279263600&period2=1542614400&interval=1d&filter=history&frequency=1d'
+
+		# filter max interval
+		scraper.filtering(browser)
+
+		# assertion for URL equality
+		self.assertEqual(browser.current_url, want_URL)
+
+		# auto scroll to bottom of page
+		scraper.scrollPage(browser, 0.5)
+
+		# exit browser
+		browser.quit()
+
+	def test_bs(self):
+		with open('./test_page.html', 'r') as f:
+			res = f.read()
+			soup = scraper.parsePage(res)
+			self.assertEqual(scraper.parsePage(res),soup)
+
+if __name__ == '__main__':
+	unittest.main()
